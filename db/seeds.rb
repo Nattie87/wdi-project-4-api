27f6226cb41b0f5b6ae1e417431a38ebe91ee6ee
Nattie87 <natalie.huitson@gmail.com>
2016-11-23 15:48:45 +0000

    changed profpic to image in seeds and tubeline to line

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ last_name: "Albarn",
 email: "damon@blur.com",
 password: "password",
 password_confirmation: "password",
-profile_picture: File.open(File.join(Rails.root, '/db/images/damon.jpg'))
+image: File.open(File.join(Rails.root, '/db/images/damon.jpg'))
 )
 
 user2 = User.create!(
@@ -19,7 +19,7 @@ last_name: "Dornan",
 email: "jamie@thefall.com",
 password: "password",
 password_confirmation: "password",
-profile_picture: File.open(File.join(Rails.root, '/db/images/jamie.jpg'))
+image: File.open(File.join(Rails.root, '/db/images/jamie.jpg'))
 )
 
 user3 = User.create!(
@@ -29,7 +29,7 @@ last_name: "Purple",
 email: "prince@purple.com",
 password: "password",
 password_confirmation: "password",
-profile_picture: File.open(File.join(Rails.root, '/db/images/prince.jpg'))
+image: File.open(File.join(Rails.root, '/db/images/prince.jpg'))
 )
 
 user4 = User.create!(
@@ -39,32 +39,32 @@ last_name: "King",
 email: "william@royal.com",
 password: "password",
 password_confirmation: "password",
-profile_picture: File.open(File.join(Rails.root, '/db/images/william.jpg'))
+image: File.open(File.join(Rails.root, '/db/images/william.jpg'))
 )
 
 post1 = user1.posts.create!(
-tubeline: "Victoria Line",
+line: "Victoria Line",
 description: "Hi, I'm Damon, you smiled at me and blushed on the Victoria Line and wow you took my breath away. Fancy coming to watch me sing in my band?",
 date: "2016-10-30",
 time: "19:15"
 )
 
 post2 = user2.posts.create!(
-tubeline: "Northern Line",
+line: "Northern Line",
 description: "Thank you to the cute girl in the spotty dress for picking up my bag items when I dropped them in the carriage. Your certainly made a shy irish man blush. Wish I got your number!",
 date: "2016-10-30",
 time: "19:15"
 )
 
 post3 = user3.posts.create!(
-tubeline: "Central Line",
+line: "Central Line",
 description: "To the girl a little worst for ware on Friday night, I helped you whilst you were being sick. It wasn't the idea situation but I would love to have a sober coffee with you!",
 date: "2016-10-21",
 time: "21:15"
 )
 
 post4 = user4.posts.create!(
-tubeline: "London Overground Line",
+line: "London Overground Line",
 description: "To the mad group of drunk women on the overground on Saturday night... you may of been pole dancing but I'll sure as hell would love a private dance to the one in the red",
 date: "2016-10-23",
 time: "21:30"
