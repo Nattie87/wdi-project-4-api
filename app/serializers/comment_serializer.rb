ddcf7051be30e706a8ac98bf39f75f7db1c33b9d
Nattie87 <natalie.huitson@gmail.com>
2017-02-20 16:46:04 +0000

    repulling app

--- app/serializers/comment_serializer.rb
+++
@@ -1,4 +0,0 @@
-class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :message, :post_id, :user_id
-  has_one :user
-end
--- app/serializers/post_serializer.rb
+++
@@ -1,5 +0,0 @@
-class PostSerializer < ActiveModel::Serializer
-  attributes :id, :line, :description, :date, :time
-  has_one :user
-  has_many :comments
-end
